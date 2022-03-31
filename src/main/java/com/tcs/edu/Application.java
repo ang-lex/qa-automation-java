fc98373f64fd93d7c1802152030e1a5575a52966
a.angapov <a.angapov@tinkoff.ru>
2022-04-01 02:45:36 +0300

    add static integer variable and add "for" cycle in Application class

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -5,6 +5,9 @@ import com.tcs.edu.printer.ConsolePrinter;
 import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
 
 class Application {
-    public static void main(String[] args) {ConsolePrinter.print(decorate("Hello world!"));
+  public static void main(String[] args) {
+    for (int i=0;i<=3;i++) {
+      ConsolePrinter.print(decorate("Hello world!"));
     }
+  }
 }
--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -6,13 +6,16 @@ import java.time.Instant;
  * Class for adding Date&Time for incoming string
  */
 public class TimestampMessageDecorator {
+  private static int value;
+
   /**
    * Add current time for incoming string
+   *
    * @param message
    * @return Current Date&Time and message
    */
   public static String decorate(String message) {
-    String result = Instant.now().toString() + " " + message;
+    String result = value++ + "  " + Instant.now().toString() + " " + message;
     return result;
   }
 }
