8537f4f6844dfebce01c983784cc1ac623a08152
a.angapov <a.angapov@tinkoff.ru>
2022-04-01 01:58:39 +0300

    add java doc for TimestampMessageDecorator class

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -2,6 +2,9 @@ package com.tcs.edu.decorator;
 
 import java.time.Instant;
 
+/**
+ * Class for adding Date&Time for incoming string
+ */
 public class TimestampMessageDecorator {
   /**
    * Add current time for incoming string
