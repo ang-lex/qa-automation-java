02071080455b52958a97e6773c38272306bc3cec
a.angapov <a.angapov@tinkoff.ru>
2022-04-24 19:12:07 +0300

    answer for 7.2-2 task

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,17 +1,19 @@
 package com.tcs.edu;
 
+import static com.tcs.edu.decorator.MessageOrder.ASC;
+import static com.tcs.edu.decorator.MessageOrder.DESC;
 import static com.tcs.edu.decorator.Severity.getRandom;
 import static com.tcs.edu.printer.MessageService.print;
 
 class Application {
   public static void main(String[] args) {
-    print(getRandom(), "Hello world!");
-    print(getRandom(), "Hello world!");
-    print(getRandom(), "Hello world!", "test");
-    print(getRandom(), "Hello world!");
-    print(getRandom(), "Hello world!", "test1", "test2");
-    print(getRandom(), "Hello world!");
-    print(getRandom(), "Hello world!");
-    print(getRandom(), "Hello world!");
+    print(getRandom(), ASC,"Hello world!");
+    print(null,ASC, "Hello world!");
+    print(getRandom(),DESC, "Hello world!", null);
+    print(getRandom(), ASC,"Hello world!");
+    print(getRandom(), DESC,"Hello world!", "test1", "test2");
+    print(getRandom(), DESC,"Hello world!");
+    print(getRandom(), ASC,null);
+    print(getRandom(), ASC,"Hello world!");
   }
 }
--- src/main/java/com/tcs/edu/printer/MessageService.java
+++ src/main/java/com/tcs/edu/printer/MessageService.java
@@ -1,8 +1,10 @@
 package com.tcs.edu.printer;
 
 import com.tcs.edu.decorator.CutDecorator;
+import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.decorator.Severity;
 
+import static com.tcs.edu.decorator.MessageOrder.ASC;
 import static com.tcs.edu.decorator.SeverityDecorator.severityLevel;
 import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
 import static com.tcs.edu.decorator.TimestampMessageDecorator.value;
@@ -11,11 +13,19 @@ import static com.tcs.edu.decorator.TimestampMessageDecorator.value;
  *
  */
 public class MessageService {
-  public static void print(Severity level, String... messages) {
+  public static void print(Severity level, MessageOrder messageService, String... messages) {
     try {
-      for (String message : messages) {
-        if (message != null) {
-          ConsolePrinter.print(CutDecorator.cutter(decorate(message) + " " + severityLevel(level)));
+      if (messageService == ASC) {
+        for (String message : messages) {
+          if (message != null) {
+            ConsolePrinter.print(CutDecorator.cutter(decorate(message) + " " + severityLevel(level)));
+          }
+        }
+      } else {
+        for (int i = messages.length-1; i >= 0; i--) {
+          if (messages[i] != null) {
+            ConsolePrinter.print(CutDecorator.cutter(decorate(messages[i]) + " " + severityLevel(level)));
+          }
         }
       }
     } catch (Exception e) {
