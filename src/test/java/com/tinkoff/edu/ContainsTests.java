2f2974dd5b8a6b7691121ca88af1e0d8f00288ca
a.angapov <a.angapov@tinkoff.ru>
2022-06-05 21:36:45 +0300

    answer for 11.2.2 task

--- src/test/java/com/tinkoff/edu/ContainsTests.java
+++ src/test/java/com/tinkoff/edu/ContainsTests.java
@@ -7,6 +7,7 @@ import com.tcs.edu.printer.ConsolePrinter;
 import org.junit.jupiter.api.Test;
 
 import static com.tcs.edu.enums.Severity.MAJOR;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ContainsTests {
@@ -19,5 +20,11 @@ public class ContainsTests {
   public void shouldSuccessAddMessageObject(){
     Message message = new Message(MAJOR,"Major message");
     assertEquals("Message {severity : MAJOR ; body : Major message}",message.toString());
+  }
+
+  @Test
+  public void shouldSuccessAddDefaultMessageObject(){
+    Message message = new Message();
+    assertThat(message).toString().equals("Message {severity : MINOR ; body : default test}");
   }
 }
